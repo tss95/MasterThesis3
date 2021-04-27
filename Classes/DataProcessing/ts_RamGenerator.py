RamGen implemented. workers and leakage fixed

Classes/DataProcessing/ts_RamGenerator.py
@@ -6,6 +6,9 @@ import sys
 import threading
 
 #from .DataHandler import DataHandler
+from tensorflow.keras.utils import GeneratorEnqueuer
+from tensorflow.keras.utils import Sequence
+import time
 
 class threadsafe_iter:
 
@@ -29,7 +32,7 @@ def threadsafe_generator(f):
         return threadsafe_iter(f(*a, **kw))
     return g
 
-@threadsafe_generator    
+#@threadsafe_generator    
 def data_generator(traces, labels, batch_size, noiseAug, num_channels = 3, is_lstm = False, norm_scale = False):
     """
     Creates a generator object which yields two arrays. One array for waveforms, and one array for labels
@@ -129,4 +132,45 @@ def ramless_data_generator(ds, y, batch_size, ramLessLoader, timeAug, num_channe
 
 def load_batch(batch, timeAug, shape, ramLessLoader):
     return ramLessLoader.load_batch(batch, timeAug, np.empty((shape)))
+
+
+def get_rambatch(num_workers, max_queue_size, **kwargs):
+    # Code similar to: https://github.com/watersink/Character-Segmentation/blob/master/data_generator.py
+    try:
+        enq = GeneratorEnqueuer(data_generator(**kwargs), use_multiprocessing = False)
+        enq.start(workers = num_workers, max_queue_size = max_queue_size)
+        gen_output = None
+        while True:
+            while enq.is_running():
+                if not enq.queue.empty():
+                    gen_output = enq.get()
+                    break
+                else:
+                    time.sleep(0.01)
+            yield gen_output
+            gen_output = None
+    finally:
+        if enq is not None:
+            enq.stop()
+
+def get_ramlessbatch(num_workers, max_queue_size, **kwargs):
+    # Code similar to: https://github.com/watersink/Character-Segmentation/blob/master/data_generator.py
+    try:
+        enq = GeneratorEnqueuer(ramless_data_generator(**kwargs), use_multiprocessing = False)
+        enq.start(workers = num_workers, max_queue_size = max_queue_size)
+        gen_output = None
+        while True:
+            while enq.is_running():
+                if not enq.queue.empty():
+                    gen_output = enq.queue.get()
+                    break
+                else:
+                    time.sleep(0.01)
+            yield gen_output
+            gen_output = None
+    finally:
+        if enq is not None:
+            enq.stop()
+        
+    
     

Classes/Modeling/CustomCallback.py
@@ -19,13 +19,15 @@ class CustomCallback(tf.keras.callbacks.Callback):
     def on_train_end(self, logs=None):
         self.val_gen = None
         gc.collect()
+        tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+        print(f"------------------Train end RAM usage: {used_m}/{tot_m} (Free: {free_m})------------------")
         
 
     def on_epoch_begin(self, epoch, logs=None):
         pass
 
     def on_epoch_end(self, epoch, logs=None):
-        val_predict = (np.asarray(self.model.predict(x = self.val_gen, steps = self.steps))).round()
+        val_predict = (np.asarray(self.model.predict(x = self.val_gen, steps = self.steps, max_queue_size = 15, workers = 8, use_multiprocessing = False))).round()
         val_targ = self.y_val[:len(val_predict)]
         _val_f = np.round(fbeta_score(val_targ, val_predict, beta = self.beta), 4 )
         self.val_fs = _val_f
@@ -33,8 +35,8 @@ class CustomCallback(tf.keras.callbacks.Callback):
 
         print(f"- val_f{self.beta}: {_val_f}")
         gc.collect()
-        #tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
-        #print(f"------------------Epoch end RAM usage: {used_m}/{tot_m} (Free: {free_m})------------------")
+        tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
+        print(f"------------------Epoch end RAM usage: {used_m}/{tot_m} (Free: {free_m})------------------")
         
     def on_test_begin(self, logs=None):
         pass

Classes/Modeling/TrainSingleModel.py
@@ -8,6 +8,7 @@ import sklearn as sk
 import tensorflow as tf
 from tensorflow.keras import mixed_precision
 from tensorflow.keras.utils import GeneratorEnqueuer
+from tensorflow.keras.utils import Sequence
 
 import os
 base_dir = '/media/tord/T7/Thesis_ssd/MasterThesis3'
@@ -74,13 +75,11 @@ class TrainSingleModel():
         
     
         
-    def fit_model(self, model, train_enq, val_enq, y_val, meier_mode = False, **p):
+    def fit_model(self, model, train_gen, val_gen, y_val, meier_mode = False, **p):
         print("Started enquers and generators")
         tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
         print(f"------------------RAM usage: {used_m}/{tot_m} (Free: {free_m})------------------")
 
-        val_gen = val_enq.get()
-        train_gen = train_enq.get()
         if meier_mode:
             fit_args = self.helper.generate_meier_fit_args(self.loadData.train, self.loadData.val, self.loadData,
                                                             p["batch_size"], p["epochs"], val_gen,
@@ -113,13 +112,11 @@ class TrainSingleModel():
             gc.collect()
             tf.config.optimizer.set_jit(True)
             mixed_precision.set_global_policy('mixed_float16')
-            model.fit(train_gen, **fit_args)
+            model.fit_generator(train_gen, **fit_args)
         except Exception:
             traceback.print_exc()
-            model = None
+            #model = None
         finally:
-            train_enq.stop()
-            val_enq.stop()
-            del train_enq, val_enq , train_gen, val_gen    
+            del train_gen, val_gen 
             gc.collect()   
             return model

RGS_DENSE.py
@@ -9,6 +9,7 @@ import h5py
 
 import tensorflow as tf
 from tensorflow.keras import mixed_precision
+from tensorflow.keras.utils import Sequence
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 os.environ['CUDA_VISIBLE_DEVICES']="0" 
@@ -51,9 +52,9 @@ mixed_precision.set_global_policy('mixed_float16')
 
 
 load_args = {
-    'earth_explo_only' : False,
+    'earth_explo_only' : True,
     'noise_earth_only' : False,
-    'noise_not_noise' : True,
+    'noise_not_noise' : False,
     'downsample' : True,
     'upsample' : True,
     'frac_diff' : 1,
@@ -70,12 +71,12 @@ handler = DataHandler(loadData)
 
 
 #- Consider editing the decay_sequences.
-"""
+
 hyper_grid = {
         "batch_size" : [64, 128, 256],
         "epochs" : [50],
         "learning_rate" : [0.1, 0.01, 0.01, 0.001, 0.0001],
-        "optimizer" : ["rmsprop", "sgd", "adam", "adam", "sgd"],
+        "optimizer" : ["rmsprop", "rmsprop", "adam", "sgd"],
         "num_layers" : [1, 1, 2, 2, 3, 4, 5],
         "units" : np.arange(50, 700, 10),
         "dropout_T_bn_F" : [True, False],
@@ -106,7 +107,7 @@ hyper_grid = {
         "activation" : ["relu"],
         "output_layer_activation" : ["sigmoid"]
 }
-
+"""
 
 model_type = "DENSE_grow"
 is_lstm = True